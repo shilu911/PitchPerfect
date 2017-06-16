c904c3f4b048a3941a83cd0f29c5d86ab7dac475
lushi <shilu911@hotmail.com>
2017-06-15 23:03:10 -0600

    Has contrains error

---
+++ PitchPerfect/RecordsoundViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Lu Shi on 2017-06-05.
+//  Copyright © 2017 lushidea. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordsoundViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordBtn: UIButton!
+    
+    @IBOutlet weak var stopRecordingBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingBtn.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingBtn.isEnabled = true
+        recordBtn.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+
+    @IBAction func stopRecording(_ sender: Any) {
+        recordBtn.isEnabled = true
+        stopRecordingBtn.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag{
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("recording was not sucessful")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! playSoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recodedAudioURL = recordedAudioURL
+        }
+    }
+}
+
---
+++ PitchPerfect/playSoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  playSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Lu Shi on 2017-06-14.
+//  Copyright © 2017 lushidea. All rights reserved.
+//
+
+import UIKit
+
+class playSoundsViewController: UIViewController {
+    
+    var recodedAudioURL: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
